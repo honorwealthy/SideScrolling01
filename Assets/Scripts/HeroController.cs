dc4e7bb35453e30f002748c7744071c80011b5ec
honorwealthy <seafood.chiu@gmail.com>
2016-06-07 17:46:20 +0800

    add mettool buster animation add hero controller

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rd2d;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rd2d = GetComponent<Rigidbody2D>();
+    }
+}
