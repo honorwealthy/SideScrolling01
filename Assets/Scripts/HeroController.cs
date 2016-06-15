3870a560f7dc971d96614fdd58f6a6755a66658d
honorwealthy <seafood.chiu@gmail.com>
2016-06-15 11:28:44 +0800

    commit player control, ground move

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -4,11 +4,42 @@ using System.Collections;
 public class HeroController : MonoBehaviour
 {
     private Animator anim;
-    private Rigidbody2D rd2d;
+    private Rigidbody2D rb2d;
+    private bool facingRight = true;
+
+    public float Speed = 5.0f;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-        rd2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //private void Update()
+    //{
+
+    //}
+
+    private void FixedUpdate()
+    {
+        var direction = Input.GetAxisRaw("Horizontal");
+        var x = Mathf.Abs(Input.GetAxis("Horizontal"));
+        anim.SetFloat("GroundSpeed", x);
+        rb2d.velocity = new Vector2(direction * x * Speed, rb2d.velocity.y);
+
+        if (direction != 0)
+            SetDirection(direction > 0);
+    }
+
+    private void SetDirection(bool isRight)
+    {
+        if (facingRight != isRight)
+        {
+            facingRight = isRight;
+
+            var theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 }
